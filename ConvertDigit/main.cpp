e02a2feb161cea7c92dea77d88233f4d9d46229e
Brent Perry <burentoperii@gmail.com>
2016-07-08 16:27:19 -0500

    Initial commit of ConvertDigit.

--- ConvertDigit/main.cpp
+++ ConvertDigit/main.cpp
@@ -6,10 +6,51 @@
 //  Copyright © 2016 Brent Perry. All rights reserved.
 //
 
-#include <iostream>
+#include "../../../std_lib_facilities.h"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+int main()
+{
+    vector<string> numbers = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+    vector<string> digits = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+    string input;
+    string output = " ";
+    bool to_continue = true;
+    bool found = false;
+    
+    while (to_continue) {
+        cout << "Please enter a number 1-9, either the digit or spelled out is fine ('|' to quit): \n";
+        cin >> input;
+        
+        if (input == "|") {
+            to_continue = false;
+            break;
+        }
+        
+        for (int i = 0; i < numbers.size(); ++i) {
+            if (numbers[i] == input) {
+                found = true;
+                output = digits[i];
+            }
+            
+        }
+        
+        if (!found) {
+            for (int j = 0; j < digits.size(); ++j) {
+                if (digits[j] == input) {
+                    found = true;
+                    output = numbers[j];
+                }
+            }
+        }
+        if (!found) {
+            cout << "You didn't enter a number between 0-9 or zero-nine.\n";
+            to_continue = false;
+            break;
+        }
+        
+        cout << "You entered '" << input << "'; which is the same as '" << output << "'.\n";
+        found = false;
+    }
+    
+    
 }
